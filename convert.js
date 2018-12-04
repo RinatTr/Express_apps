c458c07dc99353e505ae45664661f6ba82b4f9ce
RinatTr <rinatreg@gmail.com>
2018-12-03 20:43:32 -0500

    finished

--- convert.js
+++ convert.js
@@ -3,52 +3,56 @@ const app = express();
 // console.log(app);
 
 app.get('/:num/:base', (req,res) => {
-  let num = req.params.num;
+  let num = isNaN(parseInt(req.params.num)) ? req.params.num : parseInt(req.params.num);
   let base = req.params.base;
-  let result = {};
 
-  if (isNaN(num)) {
-    res.send("numbers only");
-  }
+  let result = {  "original": {"value":num, "base":null},
+                  "conversions": {"decimal":null, "binary":null, "hex":null}  };
+  let dec, bin, hex;
 
   switch (base) {
     case "bin":
-      let bin = num;
-      //convert from bin to decimal
-      let dec = parseInt(bin, 2);
-      //convert from bin to dec to hex
-      let hex = dec.toString(16);
-      result.original.value = num;
+      bin = num;
+      dec = parseInt(bin, 2);
+      hex = dec.toString(16);
       result.original.base = 2;
-      result.conversions.decimal = dec;
-      result.conversions.binary = bin;
-      result.conversions.hex = hex;
-      res.json(result); //json sends it.
       break;
     case "dec":
-      //convert from decimal to hex
-      //convert from decimal to binary
-      //store original + conversions in object
-      delete req.params.oper
-      req.params.result = num1/num2;
-      res.json(req.params);
+      dec = num;
+      bin = dec.toString(2);
+      hex = dec.toString(16);
+      result.original.base = 10;
       break;
     case "hex":
-      //convert from hex to decimal
-      //convert from hex to binary
-      //store original + conversions in object
-      delete req.params.oper
-      req.params.result = num1*num2;
-      res.json(req.params);
+      hex = num;
+      dec = parseInt(hex, 16);
+      bin = dec.toString(2);
+      result.original.base = 16;
       break;
-    case "sub":
-      delete req.params.oper
-      req.params.result = num1 - num2;
-      res.json(req.params);
     default:
       res.send("invalid response. try again: /operator/num1/num2. operators: add/sub/mul/div")
   }
+    result.conversions.decimal = dec;
+    result.conversions.binary = bin;
+    result.conversions.hex = hex;
+    res.json(result);
+})
 
+app.get('/:num/',(req, res) => {
+  let num = parseInt(req.params.num);
+
+  let result = {  "original": {"value":num, "base":null},
+                  "conversions": {"decimal":null, "binary":null, "hex":null}  };
+
+  let dec = num;
+  let bin = dec.toString(2);
+  let hex = dec.toString(16);
+  result.original.base = 10;
+
+  result.conversions.decimal = dec;
+  result.conversions.binary = bin;
+  result.conversions.hex = hex;
+  res.json(result);
 })
 
 // app.get("/*", (req,res) => {
@@ -58,5 +62,3 @@ app.get('/:num/:base', (req,res) => {
 app.listen(3000, () => {
   console.log("you are listening to port 3000")
 })
-
-// 10 - decimal , // 2 - binary, // 16 - hex
